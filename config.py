dae0dc531192812f6a7384e921c0efc827a945b2
DevWilson24 <118274743+myselfdev24@users.noreply.github.com>
2023-08-15 19:57:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hi {mention}✨</b> \n\n<b>I am #Team_MDL Offical File store Bot\n\nPermanent File Store Bot⚡️Click The Start Button 🔰</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
